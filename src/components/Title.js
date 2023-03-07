d0add461f417e092a43b1f056e4708f3bcbd67c0
AngeloDArienzo <darienzo78@gmail.com>
2023-03-07 09:41:49 +0000

    Basic page view set

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,8 +3,10 @@ import React from "react";
 const Title = () => {
     return (
         <div>
-            <h1 className="i blue ba bw1 br3 pa2 ma4 mh7 "
-            >Gallery App</h1>
+            <h1 className="i blue ba bw1 br3 pa2 ma4 mh4 avenir w-20 "
+            >Fire Gallery</h1>
+            <h2 className="" >Your photos</h2>
+            <p>Somenthing to tell</p>
         </div>
     )
 }
