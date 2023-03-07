5c6902dd9eeffaf8f0ef4c3e001e59ff2954ac3b
Angelo7788 <darienzo78@gmail.com>
2023-03-07 19:39:55 +0000

    firebase and UploadForm added

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from '@firebase/firestore'
+import {getStorage} from '@firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhk4m-y31737kj1w1aSZrdNtaJosPOsRU",
+  authDomain: "firegallery-7308a.firebaseapp.com",
+  projectId: "firegallery-7308a",
+  storageBucket: "firegallery-7308a.appspot.com",
+  messagingSenderId: "929130669825",
+  appId: "1:929130669825:web:5b3f737e74aa987ac95d00"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
