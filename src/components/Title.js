3e9bac38f7297028b5346cd6d070355d2626815a
Angelo7788 <darienzo78@gmail.com>
2023-03-12 18:15:54 +0000

    styling fix

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,9 +4,9 @@ const Title = () => {
   return (
     <div>
       <div className="fl w-third pa3">
-        <h2 className="i blue ba bw1 br3 pa1 mh6 avenir ">Firebase Gallery</h2>
+        <h2 className="i blue ba bw1 br3 pa1 mh4 avenir ">Firebase Gallery</h2>
       </div>
-      <div className="fl w-two-thirds pa3">
+      <div className="fl w-two-thirds pa4">
         <h1 className="avenir i">Your photos</h1>
       </div>
       <div>
